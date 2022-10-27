9f8f55d1fc103333503097ca679b19cf46a738ca
Kevin Stewart <kevin.g.stewart@gmail.com>
2022-10-27 09:11:45 -0400

    Update stage.sh

--- stage.sh
+++ stage.sh
@@ -2,9 +2,9 @@
 
 # Creates the Ansible folder structure
 
-mkdir ansible && cd ansible
-mkdir -p {files,inventory,library,playbooks,roles,scripts,templates,inventory/group_vars,inventory/host_vars,library/modules}
-touch inventory/group_vars/all.yaml && touch inventory/host_vars/host.yaml
-printf "[all]\nlocalhost\n" > inventory/hosts
-printf "[defaults]\nhost_key_checking\t= False\nretry_files_enabled = False\ninventory\t\t\t= ./inventory/hosts\nlibrary\t\t\t\t= ./library\nroles_path\t\t\t= ./roles\ncollections_paths\t= ./collection\n" > ansible.cfg
+mkdir ansible
+mkdir -p {ansible/files,ansible/inventory,ansible/library,ansible/playbooks,ansible/roles,ansible/scripts,ansible/templates,ansible/inventory/group_vars,ansible/inventory/host_vars,ansible/library/modules}
+touch ansible/inventory/group_vars/all.yaml && touch ansible/inventory/host_vars/host.yaml
+printf "[all]\nlocalhost\n" > ansible/inventory/hosts
+printf "[defaults]\nhost_key_checking\t= False\nretry_files_enabled = False\ninventory\t\t\t= ./inventory/hosts\nlibrary\t\t\t\t= ./library\nroles_path\t\t\t= ./roles\ncollections_paths\t= ./collection\n" > ansible/ansible.cfg
 

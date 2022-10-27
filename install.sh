01de6b5bef4c81c08d730583ed7e92c409fe0559
Kevin Stewart <kevin.g.stewart@gmail.com>
2022-10-27 10:26:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-## Requires Ubuntu
+## Requires Ubuntu and Bash
+
+export DEBIAN_FRONTEND=noninteractive 
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
+truncate -s0 /tmp/preseed.cfg
+echo "tzdata tzdata/Areas select America" >> /tmp/preseed.cfg
+echo "tzdata tzdata/Zones/Europe select Eastern" >> /tmp/preseed.cfg
+debconf-set-selections /tmp/preseed.cfg
+rm -f /etc/timezone /etc/localtime
+apt-get update && apt-get install -y tzdata
 
 apt update && apt install -y python3-pip ansible
 ansible --version

1d24b58559cf6beb6a1a0d756d03d976948ec6b0
Kevin Stewart <kevin.g.stewart@gmail.com>
2022-10-27 10:28:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ export DEBCONF_NONINTERACTIVE_SEEN=true
 
 truncate -s0 /tmp/preseed.cfg
 echo "tzdata tzdata/Areas select America" >> /tmp/preseed.cfg
-echo "tzdata tzdata/Zones/Europe select Eastern" >> /tmp/preseed.cfg
+echo "tzdata tzdata/Zones/America select Eastern" >> /tmp/preseed.cfg
 debconf-set-selections /tmp/preseed.cfg
 rm -f /etc/timezone /etc/localtime
 apt-get update && apt-get install -y tzdata
